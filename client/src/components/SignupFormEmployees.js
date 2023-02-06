206b7113b230c04ff0f5809c81f1dde7b3b6f82e
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-02-06 14:41:51 -0800

    Employee/Employer Signup

--- client/src/components/SignupFormEmployees.js
+++ client/src/components/SignupFormEmployees.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { UserContext } from "../UserContext";
 
-function SignupFormEmployees() {
+function SignupFormEmployees({ changeAuthMode }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [currentUser, setCurrentUser] = useContext(UserContext);
+
+  function handleEmployeeSignup(e) {
+    e.preventDefault();
+    const user = {
+      email,
+      password,
+    };
+    fetch("/employees", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((resp) => resp.json())
+      .then((newUser) => {
+        if (newUser?.errors) {
+          setErrors(newUser.errors);
+        } else {
+          setCurrentUser(newUser);
+          //   history.push('/')
+        }
+      });
+  }
+
   return (
-    <div>SignupFormEmployees</div>
-  )
+    <div className="Auth-form-container">
+      <form className="Auth-form" onSubmit={handleEmployeeSignup}>
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Employee Signup</h3>
+          <div className="text-center">
+            Not an Employee?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Employer
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          {errors.length > 0 && (
+            <ul style={{ color: "red" }}>
+              {errors.map((error) => (
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+          )}
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default SignupFormEmployees
+export default SignupFormEmployees;
--- client/src/components/SignupFormEmployers.js
+++ client/src/components/SignupFormEmployers.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { UserContext } from "../UserContext";
 
-function SignupFormEmployers() {
+function SignupFormEmployers({ changeAuthMode }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [currentUser, setCurrentUser] = useContext(UserContext);
+
+  function handleEmployerSignup(e) {
+    e.preventDefault();
+    const user = {
+      email,
+      password,
+    };
+    fetch("/employers", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((resp) => resp.json())
+      .then((user) => {
+        if (user?.errors) {
+          setErrors(user.errors);
+        } else {
+          setCurrentUser(user);
+          //   history.push('/')
+        }
+      });
+  }
+
   return (
-    <div>SignupFormEmployers</div>
-  )
+    <div className="Auth-form-container">
+      <form className="Auth-form" onSubmit={handleEmployerSignup}>
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Employer Signup</h3>
+          <div className="text-center">
+            Not an Employer?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          {errors.length > 0 && (
+            <ul style={{ color: "red" }}>
+              {errors.map((error) => (
+                <li key={error}>{error}</li>
+              ))}
+            </ul>
+          )}
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default SignupFormEmployers
+export default SignupFormEmployers;
