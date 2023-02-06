2bf0231803ee83943369ef9a1bff186764d0f472
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-02-06 12:10:27 -0800

    Update and create Employee actions

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,31 @@
 class EmployeesController < ApplicationController
+    
+    def create
+        employee = Employee.create!(employee_params)
+        session[:user_id] = employee.id
+        session[:is_employer] = 0
+        if employee.valid?
+            render json: employee, status: :created
+        else
+            render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        employee = Employee.find_by(id: params[:id])
+        employee.update!(employee_params)
+        if employee.id == session[:user_id]
+            render json: employee, status: :ok
+        else
+            renderjson: { errors: ["Not Authorized"]}, status: :unauthorized
+        end
+    end
+
+
+
+    private
+
+    def employee_params
+        params.permit(:first_name, :last_name, :email, :password)
+    end
 end
