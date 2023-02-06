ed004eee4bb84fa464b38405ececee6fabe02999
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-02-06 13:05:16 -0800

    Format

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,35 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-
 function Navbar() {
-    const [user, setUser] = useState('')
+  const [user, setUser] = useState("");
 
-    function handleLogout(){
-        console.log("Logged Out")
-    }
+  function handleLogout() {
+    console.log("Logged Out");
+  }
   return (
-     <div className="nav">
-  <div className="nav-inner">
-    <NavLink to="/" exact className="title">
-      BlockCV
-    </NavLink>
-  </div>
-  <div className="nav-inner">
-    {/* {!user ? null :
+    <div className="nav">
+      <div className="nav-inner">
+        <NavLink to="/" exact className="title">
+          BlockCV
+        </NavLink>
+      </div>
+      <div className="nav-inner">
+        {/* {!user ? null :
     <NavLink to='/all_projects'>
       View Projects
     </NavLink>
 } */}
-    <NavLink to="/signup" exact className="hvr-rotate">
-      Signup
-    </NavLink>
-    <NavLink to="/signup" className="hvr-rotate" onClick={handleLogout}>
-      Logout
-    </NavLink>
-  </div>
-</div>
-);
+        <NavLink to="/signup" exact className="hvr-rotate">
+          Signup
+        </NavLink>
+        <NavLink to="/signup" className="hvr-rotate" onClick={handleLogout}>
+          Logout
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
