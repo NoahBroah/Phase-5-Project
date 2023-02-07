1162b545cdf2f6d173aab09bb06a2bec2f33a36d
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-02-06 18:00:22 -0800

    display logged in user

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,19 @@
-import React, { useContext} from 'react'
-import { UserContext } from '../UserContext';
+import React, { useContext } from "react";
+import { UserContext } from "../UserContext";
 function Home() {
-
-const [user, setUser] = useContext(UserContext);
+  const [user, setUser] = useContext(UserContext);
 
   return (
-    <div>{user.first_name}</div>
-  )
+    <div>
+      {!user ? (
+        "hello"
+      ) : (
+        <div>
+          <h2>{user.first_name} {user.last_name}</h2>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
